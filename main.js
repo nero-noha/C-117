2838e8786f3eb0a6237464f95cb36254ca6b5c2e
nero-noha <114581659+nero-noha@users.noreply.github.com>
2022-12-22 19:45:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,41 @@ timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+
+function updateCanvas() {
+    background("white");
+    andom_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+    console.log(quick_draw_data_set[random_number]);
+    sketch = quick_draw_data_set[random_number];
+    document.getElementById('sketch').innerHTML = 'Sketch To be Drawn: ' + sketch;
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function draw() {
+    check_sketch();
+    if(drawn_sketch = sketch) {
+        answer_holder="set";
+        score++;
+        document.getElementById("score").innerHTML="Score: "+score;
+    }
+}
+
+function check_sketch() {
+    timer_counter++;
+    document.getElementById("timer").innerHTML="timer: "+timer_counter;
+    console.log(timer_counter);
+    if(timer_counter>400) {
+        timer_counter=0;
+        timer_check="completed";
+    }
+    if(timer_check=="completed"||answer_holder=="set") {
+        timer_check="";
+        answer_holder="";
+        updateCanvas();
+    }
+}
